b8646231601b41b033c3e1c46960c8caea1b1629
kimahro77 <kilik.972@gmail.com>
2019-01-07 23:59:15 +0100

    refonte de la home avec Bootstrap

--- app/views/header.view.php
+++ app/views/header.view.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Sonic the Hedgehog</title>
-    <link rel="icon" type="image/png" href="<?= getAbsoluteURL('images/icons/th.jpeg'); ?>"/>
+    <link rel="icon" type="image/png" href="<?= getAbsoluteURL('images/icons/th.ico'); ?>"/>
     <link rel="stylesheet" type="text/css" media="screen" href="<?= getAbsoluteURL('css/style.css'); ?>" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
